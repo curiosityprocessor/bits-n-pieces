2703dd6312c0a26c7480843b3e49f7b59e4ac56c
Chanyoung Joo <80628662+curiosityprocessor@users.noreply.github.com>
2023-10-16 19:29:12 +0900

    feat: add api skeleton code

--- nestjs/src/module/pay/pay.controller.ts
+++ nestjs/src/module/pay/pay.controller.ts
@@ -1,12 +1,56 @@
-import { Controller, Get } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Put,
+} from "@nestjs/common";
 import { PayService } from "@src/module/pay/pay.service";
 
 @Controller("pay")
 export class PayController {
   constructor(private readonly payService: PayService) {}
 
+  @HttpCode(HttpStatus.OK)
   @Get("ping")
   ping(): string {
     return this.payService.ping();
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get("orders")
+  orders(): any[] {
+    return [];
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get("transactions")
+  payments(): any[] {
+    return [];
+  }
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post("transactions")
+  createTransaction(): void {
+    return;
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Put("transactions/:transactionId")
+  updateTransaction(): void {
+    return;
+  }
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post("transactions/:transactionId/cancel")
+  cancelTransaction(): void {
+    return;
+  }
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post("transactions/callback")
+  transactionCallback(): void {
+    return;
   }
 }

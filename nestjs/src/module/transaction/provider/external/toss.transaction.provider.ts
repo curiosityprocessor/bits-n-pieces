4f49d826273d7bbdb36aa91ad6a4e33410dc23f8
Chanyoung Joo <80628662+curiosityprocessor@users.noreply.github.com>
2023-11-01 20:44:23 +0900

    feat: implement createTransaction

--- nestjs/src/module/transaction/provider/external/toss.transaction.provider.ts
+++ nestjs/src/module/transaction/provider/external/toss.transaction.provider.ts
@@ -2,19 +2,24 @@ import { Injectable, NotImplementedException } from "@nestjs/common";
 import TossPaymentApi from "@src/module/transaction/api/toss.payment.api";
 import { PostTransactionRequestDto } from "@src/module/transaction/dto/transaction";
 import { RequestToTossTransformer } from "@src/module/transaction/transformer/request-to-toss.transformer";
+import { TossToEntityTransformer } from "@src/module/transaction/transformer/toss-to-entity.transformer";
 
 @Injectable()
 class TossTransactionProvider {
   constructor(
     private readonly tossApi: TossPaymentApi,
-    private readonly transformer: RequestToTossTransformer,
+    private readonly tossTransformer: RequestToTossTransformer,
+    private readonly entityTransformer: TossToEntityTransformer,
   ) {}
 
   public async createPayment(dto: PostTransactionRequestDto) {
-    const request = this.transformer.toCreatePayment(dto);
+    const request = this.tossTransformer.toCreatePayment(dto);
     const response = await this.tossApi.createPayment(request);
-    // TODO: custom validator, post-processor
-    return response;
+    const entity = this.entityTransformer.fromCreatePayment({
+      response,
+      uid: dto.uid,
+    });
+    return entity;
   }
 
   public async updatePayment(request: any) {

e412ce51414ec6d41e0d30994377a1aa786f0332
Chanyoung Joo <80628662+curiosityprocessor@users.noreply.github.com>
2023-11-14 22:51:17 +0900

    Feature/edit request dto & swagger (#4)
    * feat: add class validator decorators
    
    * chore: update swagger decorators

--- nestjs/src/module/user/dto/create-user.dto.ts
+++ nestjs/src/module/user/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsStrongPassword,
+} from "class-validator";
 
 enum Status {
   NORMAL = "normal",
@@ -9,27 +16,29 @@ enum Status {
 export class CreateUserDto {
   @ApiProperty({
     description: "사용자ID",
-    type: String,
   })
+  @IsNotEmpty()
   uid: string;
 
   @ApiProperty({
     description: "이메일",
-    type: String,
   })
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 
-  @ApiProperty({
+  @ApiPropertyOptional({
     description: "닉네임",
-    type: String,
-    required: false,
   })
+  @IsOptional()
   nickname?: string;
 
   @ApiProperty({
     description: "비밀번호",
-    type: String,
+    example: "St0ngP@55w0rd!",
   })
+  @IsNotEmpty()
+  @IsStrongPassword()
   pw: string;
 
   @ApiProperty({
@@ -37,27 +46,36 @@ export class CreateUserDto {
     type: Number,
     default: 0,
   })
+  @IsNotEmpty()
   pwFailCount: number;
 
   @ApiProperty({
     description: "계정 잠김 여부",
+    type: Boolean,
+    default: false,
   })
+  @IsNotEmpty()
   isAccountLocked: boolean;
 
   @ApiProperty({
     description: "사용자 상태",
     enum: Status,
+    default: Status.NORMAL,
   })
+  @IsNotEmpty()
+  @IsEnum(Status)
   status: Status;
 
   @ApiProperty({
     description: "최종 로그인 일시",
     type: Date,
   })
+  @IsNotEmpty()
   lastLoginAt: Date;
 
   @ApiProperty({
     description: "생성 일시",
   })
+  @IsNotEmpty()
   createdAt: Date;
 }

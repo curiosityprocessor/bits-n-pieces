3ae21dd39c59b96593e03631589aa214f7167c5e
curiosityprocessor <curiosityprocessor@gmail.com>
2024-01-12 12:38:13 +0900

    test: reactive declarations

--- itsvelte/src/routes/Counter.svelte
+++ itsvelte/src/routes/Counter.svelte
@@ -6,6 +6,7 @@
 	const displayed_count = spring();
 	$: displayed_count.set(count);
 	$: offset = modulo($displayed_count, 1);
+	$: isEven = count % 2 === 0;
 
 	function modulo(n: number, m: number) {
 		// handle negative numbers
@@ -50,6 +51,7 @@
 		</svg>
 	</button>
 </div>
+<p>is even? <strong>{isEven}</strong></p>
 
 <style>
 	.counter {

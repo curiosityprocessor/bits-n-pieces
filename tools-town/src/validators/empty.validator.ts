d29f8d880f2870744105d74e50fda8b3b094bcc4
Chanyoung Joo <80628662+curiosityprocessor@users.noreply.github.com>
2024-05-12 11:47:56 +0900

    [tools town] feat: empty validator (#20)
    * chore: shorten script for verbose test
    
    * feat: add validator package path
    
    * chore: edit prettier of E.O.L
    
    * test: add test code for isEmpty()
    
    * feat: implement isEmpty()

---
+++ tools-town/src/validators/empty.validator.ts
@@ -0,0 +1,24 @@
+export const isEmpty = (data?: unknown): boolean => {
+  if (data == null) {
+    return true;
+  }
+
+  if (Array.isArray(data)) {
+    if (data.length === 0) {
+      return true;
+    }
+    return !data.some((e) => !isEmpty(e));
+  }
+
+  if (data instanceof Date) {
+    return false;
+  }
+
+  if (typeof data === "object") {
+    return !Object.values(data).some((e) => !isEmpty(e));
+  } else if (typeof data === "string") {
+    return !data.trim();
+  } else {
+    return false;
+  }
+};

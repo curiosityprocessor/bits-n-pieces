ca88f195feb39305895421260ec770c7fcc8754e
Chanyoung Joo <80628662+curiosityprocessor@users.noreply.github.com>
2024-09-06 00:24:31 +0900

    feat: add dynamic routers

--- playground-app/src/routes/+layout.svelte
+++ playground-app/src/routes/+layout.svelte
@@ -1,9 +1,12 @@
-<script>import "../app.css";</script>
+<script>
+  import "../app.css";
+</script>
+
 <h1>Playground</h1>
 <nav>
-    <a href="/">home</a>
-    <a href="/task">task</a>
-    <a href="/chat">chat</a>
-    <a href="/dev">dev</a>
+  <a href="/">home</a>
+  <a href="/task">task</a>
+  <a href="/chat">chat</a>
+  <a href="/dev">dev</a>
 </nav>
 <slot></slot>
---
+++ playground-app/src/routes/prompt/[id]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let data;
+</script>
+
+<h1>Prompt #{data.prompt?.id}</h1>
+<div>
+  {#each data.prompt?.messages || [] as { role, content }}
+    <p>role: {role}</p>
+    <p>content: {content}</p>
+  {/each}
+</div>

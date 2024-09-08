8546ccee28f8111c199ef91dee0c828bf9d445b5
Chanyoung Joo <80628662+curiosityprocessor@users.noreply.github.com>
2024-09-08 15:03:03 +0900

    feat: add layout data for prompts (#30)

---
+++ playground-app/src/routes/prompt/[id]/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let data;
+</script>
+
+<div class="layout">
+  <main>
+    <slot></slot>
+  </main>
+</div>
+
+<aside>
+  <h2>더보기</h2>
+  <ul>
+    {#each data.prompts as { id, alias }}
+      <li>
+        <a href="/prompt/{id}">#{id}: {alias}</a>
+      </li>
+    {/each}
+  </ul>
+</aside>
+
+<style>
+  @media (min-width: 640px) {
+    .layout {
+      display: grid;
+      grid-template-columns: 1fr 16em;
+      gap: 2em;
+    }
+  }
+</style>

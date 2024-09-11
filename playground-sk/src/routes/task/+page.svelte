9eac9eef6f26ce12e54e03f525b6edcdb2965822
Chanyoung Joo <80628662+curiosityprocessor@users.noreply.github.com>
2024-09-11 12:40:28 +0900

    feat: implement ui prototype (#36)

--- playground-sk/src/routes/task/+page.svelte
+++ playground-sk/src/routes/task/+page.svelte
@@ -1,1 +1,119 @@
-<h1>Task</h1>
+<script lang="ts">
+  import { SlideToggle } from '@skeletonlabs/skeleton';
+
+  let systemPromptUiOpen = false;
+  let parameterUiOpen = false;
+</script>
+
+<div class="flex flex-col h-full">
+  <header class="flex justify-between items-center p-4 border-b">
+    <h1 class="h1">Task</h1>
+    <SlideToggle name="help" checked>HELP</SlideToggle>
+  </header>
+
+  <!-- Data -->
+  <div class="flex flex-grow">
+    <div class="flex flex-col w-1/2 min-w-[200px] resize-x overflow-auto border-r p-4">
+      <form method="post" action="?/create">
+        <h3 class="h3 mb-4">Input</h3>
+
+        <div class="mb-4">
+          <button
+            type="button"
+            class="w-full btn-lg variant-soft p-2 rounded flex justify-between text-left"
+            on:click={() => (systemPromptUiOpen = !systemPromptUiOpen)}
+          >
+            <span>System Prompt</span>
+            {systemPromptUiOpen ? '▲' : '▼'}
+          </button>
+          {#if systemPromptUiOpen}
+            <div class="w-full mt-2">
+              <label for="systemMessage" class="label"
+                ><span>System message (optional)</span>
+                <textarea
+                  name="systemMessage"
+                  class="textarea"
+                  rows="4"
+                  placeholder="(Optional)"
+                  autocomplete="off"
+                /></label
+              >
+            </div>
+          {/if}
+        </div>
+
+        <div class="mb-4">
+          <button type="button" class="w-full btn-lg variant-soft p-2 rounded text-left"
+            >User Prompt</button
+          >
+          <div class="w-full mt-2">
+            <label class="label">
+              <span>User message</span>
+              <textarea
+                name="userMessage"
+                class="textarea"
+                rows="15"
+                placeholder=""
+                autocomplete="off"
+              />
+            </label>
+          </div>
+        </div>
+
+        <div class="mb-4">
+          <button
+            type="button"
+            class="w-full btn-lg variant-soft p-2 rounded flex justify-between text-left"
+            on:click={() => (parameterUiOpen = !parameterUiOpen)}
+          >
+            <span>Model Parameters</span>
+            {parameterUiOpen ? '▲' : '▼'}
+          </button>
+          {#if parameterUiOpen}
+            <div class="w-full mt-2">
+              <label class="label">
+                <span>Temperature</span>
+                <input name="temperature" type="range" value="75" max="100" />
+              </label>
+              <label class="label mt-4">
+                <span>Max Input Tokens</span>
+                <input
+                  name="maxInputTokens"
+                  class="input"
+                  title="Max Input Tokens"
+                  type="number"
+                  placeholder="최대 15,000 개"
+                  autocomplete="off"
+                />
+              </label>
+              <label class="label mt-4">
+                <span>Max Output Tokens</span>
+                <input
+                  name="maxOutputTokens"
+                  class="input"
+                  title="Max Output Tokens"
+                  type="number"
+                  placeholder="최대 2,048 개"
+                  autocomplete="off"
+                />
+              </label>
+            </div>
+          {/if}
+        </div>
+        <div>
+          <button type="submit" class="btn-md variant-filled-surface p-2 rounded"
+            ><span>Try it!</span></button
+          >
+        </div>
+      </form>
+    </div>
+
+    <div class="w-1/2 p-4">
+      <h3 class="h3 mb-4">Output</h3>
+      <label class="label">
+        <span>Assistant message</span>
+        <textarea class="textarea" readonly rows="20" placeholder="AI 응답값" />
+      </label>
+    </div>
+  </div>
+</div>
